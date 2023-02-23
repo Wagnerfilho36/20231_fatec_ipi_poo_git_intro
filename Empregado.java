11b5130c9f21faec739f074fa2bc0d6c4320965b
Andre Luiz <andre.gamer123@gmail.com>
2023-02-23 17:22:32 -0300

    implementacao de condicionais

--- Empregado.java
+++ Empregado.java
@@ -3,7 +3,25 @@ public class Empregado{
     private String nome;
     //variável de instância (objeto)
     private int idade;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    private int tipo;
     
+    public double calcularSalario(){
+        if (tipo == 0){
+            return salario;
+        }
+        if (tipo == 1){
+            return salario + salario * comissao;
+        }
+        if (tipo == 3){
+            return salario + bonus;
+        }
+        return 0;
+
+    }
+
     public String getNome(){
         return nome;
     }
